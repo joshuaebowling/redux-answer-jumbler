654f42335511037ce2e7eb237dd2c153fb70b937
Josh Bowling <joshuaebowling@gmail.com>
2019-01-21 20:17:25 -0400

    fixed actions import

--- src/components/QuestionAnswers.tsx
+++ src/components/QuestionAnswers.tsx
@@ -3,17 +3,26 @@ import React from "react";
 import { map, each, values, chain, value } from "lodash";
 import QuestionAnswer from "./QuestionAnswer";
 import Answer from "./Answer";
-import { Droppable } from "@shopify/draggable";
-
+import Question from "./Question";
 class QuestionAnswers extends React.Component {
   constructor(props: { collection: Array<Models.VMQuestionAnswer> }) {
     super(props);
     this.questions = chain(this.props.collection)
-      .map(qa => <QuestionAnswer key={qa.id} viewModel={qa} />)
+      .map(qa => (
+        <Question
+          key={qa.id}
+          viewModel={qa}
+          onSelect={this.props.onQuestionSelect}
+        />
+      ))
       .value();
     each(this.props.answerOrder, ao => console.log(this.props.collection[ao]));
     this.answers = map(this.props.answerOrder, (a: number) => (
-      <Answer key={a} viewModel={this.props.collection[a]} />
+      <Answer
+        key={a}
+        viewModel={this.props.collection[a]}
+        onSelect={this.props.onAnswerSelect}
+      />
     ));
     this.results = map(this.props.results, result => (
       <div>JSON.stringify(result)</div>
--- src/containers/QuestionAnswer.tsx
+++ src/containers/QuestionAnswer.tsx
@@ -1,14 +1,16 @@
 /// <reference path="../index.d.ts" />
 import { connect } from "react-redux";
-import { applyAnswer, applyQuestion } from "../actions";
+import { QuestionAnswer as actions } from "../actions";
 import { values } from "lodash";
-import QuestionAnswers from "../components/QuestionAnswers.tsx";
+import QuestionAnswers from "../components/QuestionAnswers";
+const { applyAnswer, applyQuestion } = actions;
 
 const mapStateToProps = (state: Infrastructure.IState) => ({
   collection: state.collection,
   answerOrder: state.answerOrder
 });
 
+console.log(actions);
 const mapDispatchToProps = (dispatch: Function) => {
   return {
     onQuestionSelect: (questionId: number) => {
