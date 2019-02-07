e6d7883735510cd98592e78755e466b4ead43da4
Josh Bowling <joshuaebowling@gmail.com>
2019-02-07 16:10:56 -0400

    update state of work

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 
 const About = () => (
   <div>
-    <h1>Question Answer Jumbler</h1>
+    <h1>Question Answer Jumbler - WORK IN PROGRESS</h1>
     <p>
       <Link to="/try">try it now!</Link>
     </p>
     <h3>Coming Soon...</h3>
     <ol>
-      <li>Add/Edit question/answer lists</li>
+      <li>Add/Edit question/answer lists (IN PROGRESS)</li>
       <li>Scoring Information</li>
       <li>backup/export lists</li>
     </ol>
